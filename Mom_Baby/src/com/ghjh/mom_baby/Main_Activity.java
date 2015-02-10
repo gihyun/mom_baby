653f0a7c91c10e492bfddf79cee31b68f75fd383
gihyun <kghgy92@naver.com>
2015-02-10 21:54:37 +0900

    별거한건없고 메인이 힘들어

--- Mom_Baby/src/com/ghjh/mom_baby/Main_Activity.java
+++ Mom_Baby/src/com/ghjh/mom_baby/Main_Activity.java
@@ -1,6 +1,5 @@
 package com.ghjh.mom_baby;
 
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,22 +9,23 @@ import android.widget.Button;
 
 public class Main_Activity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        Button btn_manager = (Button)findViewById(R.id.btn_manager);
-        btn_manager.setOnClickListener(new OnClickListener() {
-			
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		Button btn_manager = (Button) findViewById(R.id.btn_manager);
+		btn_manager.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Intent intent = new Intent(getBaseContext(), RegOne_Activity.class);
+				Intent intent = new Intent(getBaseContext(),
+						RegOne_Activity.class);
 				startActivity(intent);
 			}
 		});
-        
-    }
+
+	}
 
 }
