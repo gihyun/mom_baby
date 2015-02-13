db73ab986dbd298160471a29783c9f7f458e0c2c
gihyun <kghgy92@naver.com>
2015-02-13 16:01:43 +0900

    activity_diagnose.xml (성장진단->아이발달상태진단) Growth_Activity.java (Diagnose_Activity) 연결 Diagnose_Activity.java 파일

---
+++ Mom_Baby/src/com/ghjh/mom_baby/Diagnose_Activity.java
@@ -0,0 +1,15 @@
+package com.ghjh.mom_baby;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Diagnose_Activity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_diagnose);
+
+	}
+}
--- Mom_Baby/src/com/ghjh/mom_baby/Growth_Activity.java
+++ Mom_Baby/src/com/ghjh/mom_baby/Growth_Activity.java
@@ -1,7 +1,11 @@
 package com.ghjh.mom_baby;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class Growth_Activity extends Activity {
 
@@ -10,6 +14,17 @@ public class Growth_Activity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_growth);
 
+		Button btn_diagnose = (Button) findViewById(R.id.btn_diagnose);
+		btn_diagnose.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(getBaseContext(),
+						Diagnose_Activity.class);
+				startActivity(intent);
+			}
+		});
 	}
 
 }
