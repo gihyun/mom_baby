0c5e0506984b262d3dfda708ff1f19504cbedea3
gihyun <kghgy92@naver.com>
2015-02-21 12:22:25 +0900

    datepickerdialog 추가 (AddBaby1_Acitivity, Diagnose_Activity)

--- Mom_Baby/src/com/ghjh/mom_baby/AddBaby1_Activity.java
+++ Mom_Baby/src/com/ghjh/mom_baby/AddBaby1_Activity.java
@@ -1,31 +1,62 @@
 package com.ghjh.mom_baby;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import android.app.Activity;
-import android.content.Intent;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.DatePicker;
 
 public class AddBaby1_Activity extends Activity {
+	int mYear, mMonth, mDay;
+	Button btn_datePicker;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_addbaby1);
-        
-        Button btn_addbaby2 = (Button)findViewById(R.id.btn_addbaby2);
-        btn_addbaby2.setOnClickListener(new OnClickListener() {
-			
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_addbaby1);
+
+		btn_datePicker = (Button) findViewById(R.id.btn_datepicker);
+
+		Calendar cal = new GregorianCalendar();
+		mYear = cal.get(Calendar.YEAR);
+		mMonth = cal.get(Calendar.MONTH);
+		mDay = cal.get(Calendar.DAY_OF_MONTH);
+
+		updateNow();
+
+		btn_datePicker.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Intent intent = new Intent(getBaseContext(), AddBaby2_Activity.class);
-				startActivity(intent);
+				new DatePickerDialog(AddBaby1_Activity.this, mDateSetListener,
+						mYear, mMonth, mDay).show();
 			}
 		});
-    }
+	}
+
+	DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+
+		@Override
+		public void onDateSet(DatePicker view, int year, int monthOfYear,
+				int dayOfMonth) {
+			// TODO Auto-generated method stub
+			mYear = year;
+			mMonth = monthOfYear;
+			mDay = dayOfMonth;
+
+			updateNow();
+		}
+	};
+
+	void updateNow() {
+		String str = mYear + "³â " + (mMonth + 1) + "¿ù " + mDay + "ÀÏ";
+		btn_datePicker.setText(str);
+	}
 
 }
--- Mom_Baby/src/com/ghjh/mom_baby/Diagnose_Activity.java
+++ Mom_Baby/src/com/ghjh/mom_baby/Diagnose_Activity.java
@@ -1,20 +1,19 @@
 package com.ghjh.mom_baby;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.DatePicker;
 
 public class Diagnose_Activity extends Activity {
-
-	Calendar m = Calendar.getInstance();
-
-	int year = m.get(Calendar.YEAR);
-	int month = m.get(Calendar.MONTH) + 1;
-	int day = m.get(Calendar.DATE);
+	int mYear, mMonth, mDay;
+	Button btn_datePicker;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +21,42 @@ public class Diagnose_Activity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_diagnose);
 
-		Button btn_datepicker = (Button) findViewById(R.id.btn_datepicker);
-		btn_datepicker.setText(year + "년 " + month + "월 " + day + "일");
-		btn_datepicker.setOnClickListener(new OnClickListener() {
+		btn_datePicker = (Button) findViewById(R.id.btn_datepicker);
+
+		Calendar cal = new GregorianCalendar();
+		mYear = cal.get(Calendar.YEAR);
+		mMonth = cal.get(Calendar.MONTH);
+		mDay = cal.get(Calendar.DAY_OF_MONTH);
+
+		updateNow();
+
+		btn_datePicker.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-
+				new DatePickerDialog(Diagnose_Activity.this, mDateSetListener,
+						mYear, mMonth, mDay).show();
 			}
 		});
+	}
+
+	DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+
+		@Override
+		public void onDateSet(DatePicker view, int year, int monthOfYear,
+				int dayOfMonth) {
+			// TODO Auto-generated method stub
+			mYear = year;
+			mMonth = monthOfYear;
+			mDay = dayOfMonth;
+
+			updateNow();
+		}
+	};
+
+	void updateNow() {
+		String str = mYear + "년 " + (mMonth + 1) + "월 " + mDay + "일";
+		btn_datePicker.setText(str);
 	}
 }
